f253add3ae77858ca17051afbcf28e100bdd1874
Ben West <bewest@gmail.com>
2015-03-12 20:22:11 -0700

    allow it progress little further?

--- run.sh
+++ run.sh
@@ -4,6 +4,8 @@
 
 retry(){ "$@" || "$@" || "$@"; }
 
+#  echo "dash dash/sh boolean false" | sudo debconf-set-selections
+# sudo dpkg-reconfigure -f noninteractive dash
 
 echo $PATH
 sudo apt-get install -y cabal-install texlive-latex-recommended texlive-latex-extra
