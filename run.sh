f02cdd29dde9652878e6508f5cf6cd15acd03975
Ben West <bewest@gmail.com>
2015-03-12 19:57:02 -0700

    retry cabal

--- run.sh
+++ run.sh
@@ -2,9 +2,12 @@
 
 
 
+retry(){ "$@" || "$@" || "$@"; }
+
+
 echo $PATH
 sudo apt-get install -y cabal-install texlive-latex-recommended texlive-latex-extra
 cabal init -n -m
 cabal update
-cabal install pandoc pandoc-citeproc
+retry cabal install pandoc pandoc-citeproc
 
